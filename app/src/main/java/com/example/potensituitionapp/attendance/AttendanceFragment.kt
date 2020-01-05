575c320a0e80a0bb08a92e8eefee0850f5dcd3e3
KC Corp <42701333+nakamura85@users.noreply.github.com>
2020-01-05 20:46:43 +0800

    attendance update

--- app/src/main/java/com/example/potensituitionapp/attendance/AttendanceFragment.kt
+++ app/src/main/java/com/example/potensituitionapp/attendance/AttendanceFragment.kt
@@ -3,38 +3,37 @@
 package com.example.potensituitionapp.attendance
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
+import androidx.navigation.fragment.findNavController
 import com.example.potensituitionapp.R
 import com.example.potensituitionapp.databinding.FragmentAttendanceBinding
 import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.fragment_attendance.*
 
+
 open class AttendanceFragment() : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val binding: FragmentAttendanceBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_attendance, container, false)
-        return binding.root
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        val binding: FragmentAttendanceBinding = DataBindingUtil.inflate(inflater,
+            R.layout.fragment_attendance, container, false)
 
-        tblMain.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+        binding.tblMain.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabReselected(p0: TabLayout.Tab?) {
-
+                println(binding.tblMain.selectedTabPosition)
             }
 
             override fun onTabUnselected(p0: TabLayout.Tab?) {
-
+                println(binding.tblMain.selectedTabPosition)
             }
 
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-
+            override fun onTabSelected(p0: TabLayout.Tab?) {
+                println(binding.tblMain.selectedTabPosition)
             }
         })
+        return binding.root
     }
 }
