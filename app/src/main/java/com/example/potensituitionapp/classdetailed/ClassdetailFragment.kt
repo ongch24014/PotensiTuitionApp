8ff0fa71a17df0d8d96494ab432386b4ef2e4a7b
ongchunheng <ocheng.90@gmail.com>
2020-01-07 22:07:13 +0800

    designed some UI v4

--- app/src/main/java/com/example/potensituitionapp/classdetailed/ClassdetailFragment.kt
+++ app/src/main/java/com/example/potensituitionapp/classdetailed/ClassdetailFragment.kt
@@ -8,7 +8,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
@@ -17,6 +19,7 @@ import com.example.potensituitionapp.Main.MainmenuFragment
 import com.example.potensituitionapp.Main.MainmenuFragmentDirections
 
 import com.example.potensituitionapp.R
+import com.example.potensituitionapp.database.Timetable
 import com.example.potensituitionapp.database.TuitionDatabase
 import com.example.potensituitionapp.databinding.FragmentClassdetailBinding
 import kotlinx.android.synthetic.main.fragment_classdetail.*
@@ -36,11 +39,16 @@ class ClassdetailFragment : Fragment() {
             R.layout.fragment_classdetail, container, false
         )
 
+        (activity as AppCompatActivity).supportActionBar?.title = "Chapters"
+
         val application = requireNotNull(this.activity).application
         val arguments = ClassdetailFragmentArgs.fromBundle(arguments)
         val class_ID = arguments.classId
 
         val dataSource = TuitionDatabase.getInstance(application).chapterDatabaseDao
+        val ds = TuitionDatabase.getInstance(application).timetableDatabaseDao
+
+        val clasname = ds.getSpecificTimetable(class_ID)
 
         val viewModelFactory = ClassdetailViewModelFactory(class_ID, dataSource, application)
 
