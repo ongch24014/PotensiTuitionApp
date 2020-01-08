af2453ad1b57d7f3497e12c47ae7c4dc37b6bb02
ongchunheng <ocheng.90@gmail.com>
2020-01-08 18:29:25 +0800

    enrollemnt done v1

--- app/src/main/java/com/example/potensituitionapp/enrollment/BindingUtils.kt
+++ app/src/main/java/com/example/potensituitionapp/enrollment/BindingUtils.kt
@@ -2,7 +2,9 @@ package com.example.potensituitionapp.Enrollment
 
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import com.example.potensituitionapp.R
 import com.example.potensituitionapp.database.Course
 
 @BindingAdapter("courseNameString")
@@ -22,6 +24,24 @@ fun TextView.setCourseDesc(item: Course) {
 @BindingAdapter("constLayout")
 fun ConstraintLayout.setCons(item: Course) {
     item?.let {
+        if(item.courseName.equals("Biology")) {
+            background = ContextCompat.getDrawable(context, R.color.tt_monday)
+        }
 
+        else if(item.courseName.equals("Chemistry")){
+            background = ContextCompat.getDrawable(context, R.color.tt_thursday)
+        }
+
+        else if(item.courseName.equals("Physics")){
+            background = ContextCompat.getDrawable(context, R.color.tt_wednesday)
+        }
+
+        else if(item.courseName.equals("English")){
+            background = ContextCompat.getDrawable(context, R.color.design_default_color_primary)
+        }
+
+        else if(item.courseName.equals("Chinese")){
+            background = ContextCompat.getDrawable(context, R.color.tt_tuesday)
+        }
     }
 }
