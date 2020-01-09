6c1d270014775f8e0d72d60ab52bfe4652ce720c
ongchunheng <ocheng.90@gmail.com>
2020-01-10 07:28:58 +0800

    fixed bugs v2

--- app/src/main/java/com/example/potensituitionapp/MainActivity.kt
+++ app/src/main/java/com/example/potensituitionapp/MainActivity.kt
@@ -94,8 +94,8 @@ class MainActivity : AppCompatActivity() {
         val channelId = "com.example.potensituitionapp"
 
         val notfication = Notification.Builder(this@MainActivity, channelId)
-            .setContentTitle("Demo Notification")
-            .setContentText("Hi Buddy")
+            .setContentTitle("Potensi Tuition Application")
+            .setContentText("Remember to do revision!")
             .setSmallIcon(R.mipmap.ic_launcher)
             .setChannelId(channelId)
             .build()
